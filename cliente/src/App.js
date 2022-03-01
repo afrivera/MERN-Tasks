7c4952cfed23f304b16c589bd843d248303b434d
arivera <andresrfelip@hotmail.com>
2022-02-28 21:38:26 -0500

    creación de componentes y configuración de react-router-dom rutas a Login, Nuevacuenta y Proyectos

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,8 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
 
 function App() {
   return (
-    <h1>MERNTasks</h1>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/nueva-cuenta' element={<NuevaCuenta />} />
+        <Route exact path='/proyectos' element={<Proyectos />} />
+      </Routes>
+    </Router>
   );
 }
 
